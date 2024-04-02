889809b9d33a844e4558ff0e51cb6df81b73408b
joninoni <jonibeltranhayer@gmail.com>
2024-04-02 12:41:15 -0600

    diseño el formulario y creo el primer type

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,53 @@
+import { categories } from "../data/categories"
+
 const Form = () => {
     return (
-        <h2>Form</h2>
+        <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="category" className="font-bold">Categoria:</label>
+                <select 
+                    id="category"
+                    className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                >
+                   {categories.map ( category =>(
+                        <option 
+                            key={category.id}
+                            value={category.name}
+                        >
+                                {category.name}
+                        </option>
+                   ))}
+                </select>
+            </div>
+
+
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="activity" className="font-bold">Actividad:</label>
+                <input 
+                    id="activity"
+                    type="text"
+                    className="border border-slate-300 rounded-lg p-2"
+                    placeholder="Ejem. Comida, Jugo de Naranga, Ensalada, Ejercicio, Pesas, Bicicleta"
+                />
+            </div>
+
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="calories" className="font-bold">Calorias:</label>
+                <input 
+                    id="calories"
+                    type="number"
+                    className="border border-slate-300 rounded-lg p-2"
+                    placeholder="Calorias ejemplo. 300 o 500"
+                />
+            </div>
+            
+            <input
+                type="submit"
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+                value="Guardar Comida O Guardar Ejercicio"
+            />
+        </form>
     )
 }
 

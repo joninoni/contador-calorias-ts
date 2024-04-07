948fda2a0fb3d9e6bca7b023c9e93e08352d0e51
joninoni <jonibeltranhayer@gmail.com>
2024-04-06 18:48:27 -0600

    muestro un texto condicional en base si hay actvidades o no

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -13,45 +13,49 @@ const ActivityList = ({activities,dispatch} : ActivityListProps) => {
 
     const nameCategory = useMemo( () => (category : Activity["category"]) => categories.map( cat => cat.id === category ? cat.name : "")  ,[])
 
-    
+    const isEmptyActivities = useMemo( () => activities.length ===0 ,[activities] )
 
     return (
         <>
-            <h2 className=" text-4xl text-center text-slate-600 font-bold">Comida Y Actividades</h2>
-            
-            {activities.map( activity => (
-                <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
-                    <div className=" space-y-2 relative">
-                        
-                        <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? "bg-lime-500" : "bg-orange-500"}`}>{nameCategory(activity.category)}</p>
-
-                        <p className=" text-2xl font-bold pt-5">{activity.name}</p>
-
-                        <p className=" font-black text-4xl text-lime-500">
-                            {activity.calories} {""}
-                            <span>Calorias</span>
-                        </p>
+            <h2 className=" text-4xl text-center text-slate-600 font-bold">Comida Y Actvidades</h2>
+            {isEmptyActivities ? 
+                <p className="text-center my-5">
+                    No hay actividades aun...
+                </p> : 
+                activities.map( activity => (
+                    <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
+                        <div className=" space-y-2 relative">
+                            
+                            <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? "bg-lime-500" : "bg-orange-500"}`}>{nameCategory(activity.category)}</p>
+    
+                            <p className=" text-2xl font-bold pt-5">{activity.name}</p>
+    
+                            <p className=" font-black text-4xl text-lime-500">
+                                {activity.calories} {""}
+                                <span>Calorias</span>
+                            </p>
+                        </div>
+    
+                        <div className="flex gap-5 items-center">
+                            <button
+                                onClick={() => dispatch( {type:"set-actvityId",payload:{id:activity.id}} )}
+                            >
+                                <PencilSquareIcon
+                                    className="h-8 w-8 text-gray-800"
+                                />
+                            </button>
+    
+                            <button
+                                onClick={() => dispatch( {type:"delete-activity",payload:{id:activity.id}}) }
+                            >
+                                <TrashIcon
+                                    className="h-8 w-8 text-red-800"
+                                />
+                            </button>
+                        </div>
                     </div>
-
-                    <div className="flex gap-5 items-center">
-                        <button
-                            onClick={() => dispatch( {type:"set-actvityId",payload:{id:activity.id}} )}
-                        >
-                            <PencilSquareIcon
-                                className="h-8 w-8 text-gray-800"
-                            />
-                        </button>
-
-                        <button
-                            onClick={() => dispatch( {type:"delete-activity",payload:{id:activity.id}}) }
-                        >
-                            <TrashIcon
-                                className="h-8 w-8 text-red-800"
-                            />
-                        </button>
-                    </div>
-                </div>
-            ))}
+                ))
+            }   
         </>
     )
 }

b436199dde7a0283cbe6c2ed618da60f4c8a7552
joninoni <jonibeltranhayer@gmail.com>
2024-04-08 18:43:56 -0600

    creo el componente de calorias

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from "react"
+import CaloriesDisplay from "./CaloriesDisplay"
 import { Activity } from "../types"
 
 type CalorieTrackerProps = {
@@ -10,26 +11,28 @@ const CalorieTracker = ({state} : CalorieTrackerProps) => {
 
     const caloriesConsumed = useMemo( ()=> state.reduce( (acc,el) => el.category === 1 ? acc + el.calories : acc,0 ) ,[state])
 
-    const caloriesBurned= useMemo( ()=> state.reduce( (acc,el) => el.category === 2 ? acc + el.calories : acc,
+    const caloriesBurned = useMemo( ()=> state.reduce( (acc,el) => el.category === 2 ? acc + el.calories : acc,
     0 ) ,[state])
+
+    const netCalories = useMemo( ()=> caloriesConsumed - caloriesBurned ,[caloriesConsumed,caloriesBurned])
 
     return (
         <>
             <h2 className="text-4xl font-black text-white text-center">Resumen de Calorias</h2>
 
             <div className="flex flex-col item-center gap-5 mt-10 md:flex-row md:justify-between">
-                <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-                    <span className="font-black text-6xl text-orange">{caloriesConsumed}</span>
-                    Consumidas
-                </p>
-                <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-                    <span className="font-black text-6xl text-orange">{caloriesBurned}</span>
-                    Gastadas
-                </p>
-                <p className=" text-orange-800 font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-                    <span className="font-black text-6xl text-orange">{caloriesConsumed - caloriesBurned}</span>
-                    Diferencia
-                </p>
+                <CaloriesDisplay
+                    calories={caloriesConsumed}
+                    text={"Consumidas"}
+                />
+                <CaloriesDisplay
+                    calories={caloriesBurned}
+                    text={"Ejercicio"}
+                />
+                <CaloriesDisplay
+                    calories={netCalories}
+                    text={"Diferencia"}
+                />
             </div>
         </>
     )
